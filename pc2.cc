50766d802c9665350b57ef5430cc71910364cb49
hmyan90 <34290458+hmyan90@users.noreply.github.com>
2018-10-18 19:35:25 -0700

    Update pc2.cc

--- pc2.cc
+++ pc2.cc
@@ -49,8 +49,8 @@ void produce(unsigned int *seed)
     t.item = rand_r(seed) % NUM_ITEM;
 
     bool flag = true;
-    global_mutex_q.lock();
     while (flag == true) {
+        global_mutex_q.lock();
         if (q.size() <= 100) {
 	    flag = false;
 	} else {
@@ -66,9 +66,9 @@ int consume()
     int num_ops = 0;
 	
     bool flag = true;
-    global_mutex_q.lock();
     while (flag == true) {
-        if (q.empty()) {
+        global_mutex_q.lock();
+        if (!q.empty()) {
 	    flag = false;
 	} else {
 	    global_mutex_q.unlock();
