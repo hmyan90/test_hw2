e6384e7448927ed3be1f6c1810b65a9eb4ce931a
hmyan90 <34290458+hmyan90@users.noreply.github.com>
2018-10-19 13:29:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ import commands
 commands.getstatusoutput('./pc1 > pc_1.txt')
 
 for question in [2,3,4]:
-	for thread in [1,2,4,8,16,32]:
-		exec_pc = "./pc%d" %question
-		output = "pc_%d_%d.txt" %(question, thread)
-		if question == 3:
-			commands.getstatusoutput('%s %d 0 > %s' %(exec_pc, thread, output))
-		else:
-			commands.getstatusoutput('%s %d > %s' %(exec_pc, thread, output))
+        for thread in [1,2,4,8,16,32]:
+                exec_pc = "./pc%d" %question
+                output = "pc_%d_%d.txt" %(question, thread)
+                if question == 3:
+                        commands.getstatusoutput('%s %d 0 > %s' %(exec_pc, thread, output))
+                else:
+                        commands.getstatusoutput('%s %d > %s' %(exec_pc, thread, output))
 
 
